457d8287ac73e189dcd2cca4228601c335dfb976
Manzala Kazmi-Shah <manzala@Manzalas-MacBook-Pro.local>
2018-04-09 19:56:35 -0400

    added template , statics folder, and added urls inside of VUEWIZ

--- Vue/Vue/urls.py
+++ Vue/Vue/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'Vue.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', include('VueWiz.urls')),
     url(r'^admin/', include(admin.site.urls)),
+
 ]
