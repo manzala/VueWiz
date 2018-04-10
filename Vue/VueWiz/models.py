045f193b6d0795cda303e84c2d67affbb8a35b24
Manzala Kazmi-Shah <manzala@Manzalas-MacBook-Pro.local>
2018-04-09 22:19:55 -0400

    added signup view and html, needs bootstrap

--- Vue/VueWiz/models.py
+++ Vue/VueWiz/models.py
@@ -1,7 +1,22 @@
 from __future__ import unicode_literals
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 from django.db import models
 
 # Create your models here.
 
 
+class UserRegistrationForm(forms.Form):
+    username = forms.CharField(
+        required = True,
+        label = 'Username',
+        max_length = 32
+	)
+    password = forms.CharField(
+        required = True,
+        label = 'Password',
+        max_length = 32,
+        widget = forms.PasswordInput()
+    )
--- Vue/VueWiz/views.py
+++ Vue/VueWiz/views.py
@@ -1,5 +1,34 @@
 from django.shortcuts import render
+from django.shortcuts import render
+
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from models import UserRegistrationForm
 
 # Create your views here.
 def index(request):
     return render(request, template_name="index.html", context={"done": True})
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            userObj = form.cleaned_data
+            print(userObj)
+            username = userObj['username']
+            password = userObj['password']
+            if not (User.objects.filter(username=username).exists()):
+                User.objects.create_user(username, email=None, password=password)
+                user = authenticate(username=username, password=password)
+                login(request,user)
+                return HttpResponseRedirect('signup')
+            else:
+                raise forms.ValidationError('Looks like a username with that email or password already exists')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'signup.html', {'form': form})
+
